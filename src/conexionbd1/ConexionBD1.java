0ca5fd8aef927394eb4e32d895d8986eec88e9b9
SuperK92 <kelly.suarez17@gmail.com>
2018-01-15 20:37:22 +0000

    Ejercicio 7

---
+++ src/conexionbd1/ConexionBD1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexionbd1;
+
+/**
+ *
+ * @author alumno_n
+ */
+public class ConexionBD1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
